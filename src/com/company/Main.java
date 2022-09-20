74639e231f1a8660556e69fef4af0db1d9e820ec
ayanalier <kimayana450@gmail.com>
2022-09-20 23:55:39 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+        public class Main {
+            public static void main(String[] args) {
+                sayHello("Аяна");
+                String ClearSunnyDay = "2 Day";
+                String word = " Day ";
+                System.out.println(ClearSunnyDay + word + Nam);
+                if (Nam < 0) {
+                    System.out.println("Вы сохранили отрицательное число");
+                } else if (Nam > 0) {
+                    System.out.println("Вы сохранили положительное число");
+                } else {
+                    System.out.println("Вы сохранили ноль");
+                }
+            }
+            public static final int Nam = 2;
+            public static void sayHello(String name){
+                System.out.println("Введите ваше имя:");
+                System.out.println("Аяна");
+                if (name== "Аяна"){
+                    System.out.println("Привет!!! " + name);
+                }
+
+                }
+            }
+
+
